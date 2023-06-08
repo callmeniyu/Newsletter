f384cb29de85fcc282e2967680d23be1c16b9752
callmeniyu <getniyashere@gmail.com>
2023-06-08 08:57:10 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,77 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = ("request");
+const https = require("https");
+require("dotenv").config();
+
+console.log()
+
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function (req, res) {
+    const firstName = req.body.FirstName;
+    const lasttName = req.body.LastName;
+    const email = req.body.Email;
+
+    const data = {
+        members: [
+            {
+                email_address:email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lasttName,
+
+                }
+            }
+        ]
+    };
+
+    const jsonData = JSON.stringify(data);
+
+    // https://us9.api.mailchimp.com/3.0/lists/8283cf3895
+    // https://us9.api.mailchimp.com/3.0/8283cf3895
+    
+    const url = "https://us9.api.mailchimp.com/3.0/lists/8283cf3895";
+
+    const options = {
+        method: "POST",
+        auth: "niyus:"+process.env.API_KEY+""
+    }
+
+    const request = https.request(url, options, function (response) {
+        if (response.statusCode === 200) {
+            res.sendFile(__dirname + "/success.html")
+        } else {
+            res.sendFile(__dirname + "/failure.html")
+        }
+        response.on("data", function (data) {
+            const datas = (JSON.parse(data));
+           
+        })
+    })
+
+    request.write(jsonData);
+    request.end();
+
+})
+
+app.post("/failure", function (req,res) {
+    res.redirect("/");
+})
+
+app.listen(process.env.PORT || 3000,function () {
+    console.log("Server is running at localport 3000");
+})
+
+
+// WEBSITE URL
+// https://newsletter-emails.onrender.com/
